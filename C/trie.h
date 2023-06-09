655ea3fb856df3ddf4ac3a2eea7b4f1bc0e41bbe
Speanight <ltewithl@gmail.com>
2023-06-09 14:45:06 +0200

    struct par realisateurs fonctionne

--- C/trie.h
+++ C/trie.h
@@ -1,7 +1,7 @@
 #ifndef PROJETCIR1_GROUPE_6_TRIE_H
 #define PROJETCIR1_GROUPE_6_TRIE_H
 
-#define ALPHABET 26
+#define ALPHABET 27
 #define MAX_REALISATEUR_SIZE 64
 #define MAX_GENRE_SIZE 24
 #define MAX_TITRE_SIZE 64
@@ -24,11 +24,12 @@ int charToInt(char c);
 
 bool lastLetter(struct NodeTrie* trie);
 bool getIsWord(struct NodeTrie* trie);
+char* toLower(char* str);
 
 struct NodeTrie* buildTrieFromFile(char* nameFile);
 
 struct NodeTrie* createEmptyNodeTrie();
-void insertWord(struct NodeTrie* trie, char* word);
+void insertWord(struct NodeTrie* trie, struct Movie* m);
 void deleteWord(struct NodeTrie* trie, char* word);
 bool findWord(struct NodeTrie* trie, char* word);
 void deleteNodeTrie(struct NodeTrie** trie);     // free d'une feuille.
