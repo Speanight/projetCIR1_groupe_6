7b2dd718fe1473d7e891c2ab4346a0986161f43a
Clément <clement.robin@isen-ouest.yncrea.fr>
2023-06-09 16:31:23 +0200

    qd

--- filmotheque.js
+++ filmotheque.js
@@ -15,7 +15,7 @@ function disabledSearch(){
     function disabledBut2() {
         if (input2.checkValidity() == false) {
             but2.innerHTML = '<svg class="send" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-60q142.375 0 241.188-98.812Q820-337.625 820-480q0-60.662-21-116.831Q778-653 740-699L261-220q45 39 101.493 59.5Q418.987-140 480-140ZM221-261l478-478q-46-39-102.169-60T480-820q-142.375 0-241.188 98.812Q140-622.375 140-480q0 61.013 22 117.507Q184-306 221-261Z"/></svg>';
-
+            but2.classList.add('disabled');
         } else {
             but2.innerHTML = '<svg class="send" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M120-160v-640l760 320-760 320Zm60-93 544-227-544-230v168l242 62-242 60v167Zm0 0v-457 457Z"/></svg>';
         }
