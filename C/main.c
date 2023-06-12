8ef46f3667136808985a00f4ab4f15643c62d56c
Speanight <ltewithl@gmail.com>
2023-06-12 09:42:26 +0200

    export files and read of requests done

--- C/main.c
+++ C/main.c
@@ -9,22 +9,63 @@
 #include "dictionary.h"
 
 int main() {
-    struct List* l = createEmptyList();
-
+//    struct List* l = createEmptyList();
 //    struct Movie* m = createMovie("rea", "titre", 64, "genre");
-
-//    struct NodeTrie* t = buildTrieFromFile("../DB/BD_small-maisEnMieux.txt");
+//    struct NodeTrie* t = buildTrieFromFile("../DB/BD_small.txt");
 
     struct Database* db = createDataBase("../DB/BD_small.txt");
 
-    exportFromDuration(db, 115, "../test.txt");
+    bool endServer = false;
+    FILE* request;
+
+//    exportFromDuration(db, 115, "../test.txt");
+//    exportFromDuration(db, 115, "../test.txt");
+
+    while(!endServer) {
+        char line[64];
+        char* command;
+        char* argumentStr;
+        int argumentInt;
+        int compare;
+
+        request = fopen("../../request.txt", "r");
+        if (request != NULL) {
+            fgets(line, sizeof(line), request);
+            command = strtok(line, ";");
+
+            compare = strcmp(command, "exportFromDuration");
+            if (compare == 0) {
+                printf("export from duration!\n");
+                argumentInt = atoi(strtok(NULL, ";"));
+                printf("argumentInt = %d\n", argumentInt);
+                exportFromDuration(db, argumentInt, "../result.txt");
+            }
+            else {
+                compare = strcmp(command, "exportAllFromRealisateurs");
+                if (compare == 0) {
+                    printf("export from realisateurs!\n");
+                    argumentStr = strtok(NULL, ";");
+                    exportAllFromRealisateurs(db->triParRealisateurs, argumentStr, "../../result.txt");
+                }
+                else {
+                    compare = strcmp(command, "endServer");
+                    if (compare == 0) {
+                        endServer = true;
+                    }
+                }
+            }
+            fclose(request);
+            remove("../../request.txt");
+        }
+    }
+
+//    exportFromDuration(db, 115, "../test.txt");
 
 //    struct Dictionary* d = createEmptyDictionary();
 //    updateDictionary("../DB/BD_small.txt", d);
+//    findAllMovies(db->triParRealisateurs, "l", l);
 
-    findAllMovies(db->triParRealisateurs, "lee", l);
-
-    exportAllFromRealisateurs(db->triParRealisateurs, "lee", "../test.txt");
+//    exportAllFromRealisateurs(db->triParRealisateurs, "l", "../test.txt");
 
     return 0;
 }
